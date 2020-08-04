c2a3d6315d91928b31c2f42460e7346f9bab2333
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-08-04 15:43:56 +0300

    fix

--- scripts/component.js
+++ scripts/component.js
@@ -1,1 +1,12 @@
-export default (props) => <div>Hello From React component {props.testProp}</div>
+export default (props) => {
+  const [count, setCount] = React.useState(0)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  )
+}
