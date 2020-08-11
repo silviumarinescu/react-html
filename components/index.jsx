ca6630bfe8f384756b992bbcd907a215072b63e2
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-08-11 18:25:20 +0300

    fix

--- components/index.jsx
+++ components/index.jsx
@@ -10,13 +10,15 @@ const GlobalStyle = styled.createGlobalStyle`
 const App = () => {
   const initialState = {
     gridSize: {
-      w: 20,
-      h: 20,
+      w: 15,
+      h: 15,
     },
     velocity: {
       x: 1,
       y: 0,
     },
+    direction: "right",
+    prevDirection: "right",
     fruit: {
       x: 0,
       y: 0,
@@ -25,7 +27,7 @@ const App = () => {
       x: 0,
       y: 0,
     },
-    tailSize: 5,
+    tailSize: 3,
     trail: [],
     gameOver: false,
     timeout: null,
@@ -36,22 +38,134 @@ const App = () => {
 
   const StyledApp = styled.div`
     .grid {
-      width: 100vw;
-      height: 100vh;
+      width: ${state.gridSize.w * 64}px;
+      height: ${state.gridSize.h * 64}px;
       display: grid;
       grid-template-columns: repeat(${state.gridSize.w}, 1fr);
       grid-template-rows: repeat(${state.gridSize.h}, 1fr);
       grid-auto-flow: row;
       .cell {
         background: black;
-        &.snake {
-          background: green;
+        &.snake,
+        &.fruit {
+          position: relative;
+          &:after {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-size : ${64*5}px ${64*4}px;
+            background-image: url("images/snake-graphics.png");
+          }
+        }
+
+        &.head-right {
+          &:after {
+            background-position: 100% 0;
+          }
+        }
+        &.head-left {
+          &:after {
+            background-position: -${64 * 3}px -${64 * 1}px;
+          }
+        }
+        &.head-up {
+          &:after {
+            background-position: -${64 * 3}px -${64 * 0}px;
+          }
+        }
+        &.head-down {
+          &:after {
+            background-position: -${64 * 4}px -${64 * 1}px;
+          }
+        }
+
+        &.tail-right {
+          &:after {
+            background-position: -${64 * 4}px -${64 * 2}px;
+          }
+        }
+        &.tail-left {
+          &:after {
+            background-position: -${64 * 3}px -${64 * 3}px;
+          }
+        }
+        &.tail-up {
+          &:after {
+            background-position: -${64 * 3}px -${64 * 2}px;
+          }
+        }
+        &.tail-down {
+          &:after {
+            background-position: -${64 * 4}px -${64 * 3}px;
+          }
         }
         &.fruit {
-          background: red;
+          &:after {
+            background-position: 0 100%;
+          }
         }
-        &.head {
-          background: pink;
+        &.body-left-left {
+          &:after {
+            background-position: -${64 * 1}px -${64 * 0}px;
+          }
+        }
+        &.body-left-up {
+          &:after {
+            background-position: -${64 * 0}px -${64 * 1}px;
+          }
+        }
+        &.body-left-down {
+          &:after {
+            background-position: -${64 * 0}px -${64 * 0}px;
+          }
+        }
+        &.body-down-down {
+          &:after {
+            background-position: -${64 * 2}px -${64 * 1}px;
+          }
+        }
+        &.body-down-left {
+          &:after {
+            background-position: -${64 * 2}px -${64 * 2}px;
+          }
+        }
+        &.body-down-right {
+          &:after {
+            background-position: -${64 * 0}px -${64 * 1}px;
+          }
+        }
+        &.body-up-up {
+          &:after {
+            background-position: -${64 * 2}px -${64 * 1}px;
+          }
+        }
+        &.body-up-right {
+          &:after {
+            background-position: -${64 * 0}px -${64 * 0}px;
+          }
+        }
+        &.body-up-left {
+          &:after {
+            background-position: -${64 * 2}px -${64 * 0}px;
+          }
+        }
+        &.body-right-right {
+          &:after {
+            background-position: -${64 * 1}px -${64 * 0}px;
+          }
+        }
+        &.body-right-down {
+          &:after {
+            background-position: -${64 * 2}px -${64 * 0}px;
+          }
+        }
+        &.body-right-up {
+          &:after {
+            background-position: -${64 * 2}px -${64 * 2}px;
+          }
         }
       }
     }
@@ -81,17 +195,35 @@ const App = () => {
   };
 
   const render = () => {
-    state.timeout = setTimeout(renderFrame, (state.gridSize.w * state.gridSize.h / state.tailSize) * 10);
+    state.timeout = setTimeout(
+      renderFrame,
+      ((state.gridSize.w * state.gridSize.h) / state.tailSize) * 10
+    );
   };
 
   const renderFrame = () => {
     if (state.gameOver) return;
-    state.trail.push({ x: state.head.x, y: state.head.y });
+
+    state.prevDirection = state.direction;
+    if (state.velocity.x == -1) state.direction = "left";
+    else if (state.velocity.x == 1) state.direction = "right";
+    else if (state.velocity.y == -1) state.direction = "up";
+    else if (state.velocity.y == 1) state.direction = "down";
+
+    state.trail.push({
+      x: state.head.x,
+      y: state.head.y,
+      direction: state.direction,
+      prevDirection: state.prevDirection,
+      isTail: false,
+    });
+    state.prevDirection = state.direction;
     if (state.head.x == state.fruit.x && state.head.y == state.fruit.y) {
       state.tailSize++;
       setRandomFruit();
     }
     if (state.trail.length > state.tailSize) state.trail.shift();
+    state.trail[0].isTail = true;
 
     state.head.x += state.velocity.x;
     state.head.y += state.velocity.y;
@@ -155,7 +287,6 @@ const App = () => {
       window.removeEventListener("keydown", keyPress, false);
     };
   }, []);
-
   return (
     <StyledApp>
       <GlobalStyle />
@@ -165,12 +296,20 @@ const App = () => {
             [...new Array(state.gridSize.h).fill(null)].map((cell, j) => (
               <div
                 className={`cell${
-                  state.head.x == j && state.head.y == i
-                    ? " head"
+                  state.trail.find((it) => it.x == j && it.y == i)
+                    ? state.trail.find((it) => it.x == j && it.y == i).isTail
+                      ? ` snake tail-${state.trail.find((it) => it.x == j && it.y == i).direction}`
+                      : ` snake body-${
+                          state.trail.find((it) => it.x == j && it.y == i)
+                            .prevDirection
+                        }-${
+                          state.trail.find((it) => it.x == j && it.y == i)
+                            .direction
+                        }`
+                    : state.head.x == j && state.head.y == i
+                    ? ` snake head-${state.direction}`
                     : state.fruit.x == j && state.fruit.y == i
                     ? " fruit"
-                    : state.trail.find((it) => it.x == j && it.y == i)
-                    ? " snake"
                     : ""
                 }`}
                 key={j}
